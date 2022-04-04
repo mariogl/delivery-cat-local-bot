ac63bbbf97224774b4018c03f0fee91160025dc1
Mario González <mario@mariogl.com>
2022-04-04 19:52:54 +0200

    Fix: use username when there is no nickname

--- index.js
+++ index.js
@@ -16,7 +16,13 @@ const { Client, Intents } = require("discord.js");
 
 const extractInfo = async (messageData) => {
   const message = messageData.content.trim();
-  let { nickname } = await messageData.guild.members.fetch(messageData.author);
+  let {
+    nickname,
+    // eslint-disable-next-line prefer-const
+    user: { username },
+  } = await messageData.guild.members.fetch(messageData.author);
+  debug(chalk.cyanBright("Nickname: ", nickname || username));
+  nickname = nickname || username;
   nickname = nickname
     .normalize("NFD")
     .replace(/\p{Diacritic}/gu, "")
@@ -47,12 +53,10 @@ const cloneRepo = (repoURL, channel, category, nickname) => {
     }
 
     process.chdir(folder);
-
     if (fs.existsSync(path.join(folder, nickname))) {
       debug(chalk.yellow(`El repo ${nickname} ya existe localmente`));
     } else {
       debug(chalk.yellow(`Clonando repo ${repoURL}`));
-
       const stdoutGitClone = execSync(`git clone ${repoURL} ${nickname}`, {
         encoding: "utf-8",
       });
@@ -95,7 +99,12 @@ client.on("ready", async () => {
   );
   const messages = await challengeChannel.messages.fetch();
   messages.forEach(async (message) => {
-    const { message: content, nickname } = await extractInfo(message);
+    // eslint-disable-next-line prefer-const
+    let { message: content, nickname } = await extractInfo(message);
+    const firstLineBreakPosition = content.indexOf("\n");
+    if (firstLineBreakPosition !== -1) {
+      content = content.substring(0, firstLineBreakPosition);
+    }
     debug(chalk.green(`Parseando mensaje de ${nickname}: ${content}`));
     if (
       (content.startsWith("Repo:") ||
